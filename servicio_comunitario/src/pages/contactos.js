3f5bf2e6ec96ec9673fbc48c4d732cb457406a68
moises <1128moises@gmail.com>
2021-09-30 20:10:24 -0400

    REFACTOR(contac/page)

--- servicio_comunitario/src/pages/contactos.js
+++ servicio_comunitario/src/pages/contactos.js
@@ -1,118 +1,169 @@
-import React, { Component } from 'react';
-import { Card ,Col, Container, Row} from 'react-bootstrap';
-import telephone from '../assets/smartphone.png';
-import mail from '../assets/email.png';
-import Background from '../assets/back.jpg';
-import facebook from '../assets/facebook.png';
-import instagram from '../assets/instagram.png';
+import React, { Component } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import telephone from "../assets/smartphone.png";
+import mail from "../assets/email.png";
+import Background from "../assets/back.jpg";
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
 
 var sectionStyle = {
-    backgroundSize: "cover",
-          height: "100vh",
-    backgroundImage: `url(${Background})`
-  };
+  backgroundSize: "cover",
+  height: "50vh",
+  backgroundImage: `url(${Background})`,
+};
 
 class Contactos extends Component {
-    render() {
-        return (
-            <section style={ sectionStyle }>
-            <Container style={{maxWidth:800, alignItems:'center', paddingTop:30}}>
-             
-                <Row >
-                
-                <Col> 
-                <Card style={{marginBottom:40, height:400}}>
-                    <Card.Body>
-                        <Row style={{marginTop:10}}>
-                            <Col style={{textAlign:"right"}}> 
-                                <Card.Title style={{textAlign:"left"}}>Telefonos</Card.Title> 
-                            </Col> 
-                            <Col style={{textAlign:"right"}}> 
-                                <img src={telephone} alt="tlf" width="40" />
-                            </Col> 
-                        </Row>
-                        
-                        <Card.Text style={{textAlign:"left"}}>
-                            Teléfonos: 0412-3646441
-                        </Card.Text>
-                        <Card.Text style={{textAlign:"left", marginTop:20}}>
-                            Teléfonos: 0412-3646441
-                        </Card.Text>
-                        <Card.Text style={{textAlign:"left", marginTop:20}}>
-                            Teléfonos: 0412-3646441
-                        </Card.Text>
-                        <Row style={{marginTop:30}}>
-                            <Col style={{textAlign:"right"}}> 
-                                <Card.Title style={{textAlign:"left"}}>Correos</Card.Title> 
-                            </Col> 
-                            <Col style={{textAlign:"right"}}> 
-                                <img src={mail} alt="tlf" width="40" />
-                            </Col> 
-                        </Row>
-                        <Card.Text style={{textAlign:"left"}}>
-                             yjmendez10@gmail.com
-                        </Card.Text>
-                        <Card.Text style={{textAlign:"left", marginTop:20}}>
-                             yjmendez10@gmail.com
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer> </Card.Footer>
-                </Card>
-                </Col>
+  render() {
+    return (
+      <section style={sectionStyle}>
+        <div
+          style={{
+            fontSize: 40,
+            color: "#ffffff",
+            fontWeight: "bold",
+            paddingTop: 50,
+          }}
+        >
+          <lable>
+            Pagina de Contacto
+            <p>
+              Esta es la pagina por la cual usted se podra contactar con el
+              Colegio
+            </p>
+          </lable>
+        </div>
 
-                <Col> 
-                <Card style={{marginBottom:40,height:400}}>
-                    <Card.Body style={{marginTop:10}}>
+        <Container style={{ paddingTop: 220 }}>
+          <Row>
+            <Col style={styles.card}>
+              <Card style={styles.c}>
+                <Card.Body>
+                  <Card.Title>Telefonos</Card.Title>
+                  <Col>
+                    <Card.Text style={{ textAlign: "center", marginTop: 20 }}>
+                      yjmendez10@gmail.com
+                    </Card.Text>
+                  </Col>
+                  <Row>
+                    <Col style={{ marginTop: 20 }}>
+                      <Card.Link
+                        target="_blank"
+                        href="https://www.facebook.com"
+                      >
+                        Facebook
+                      </Card.Link>
+                    </Col>
+                    <Col style={{ paddingTop: 10 }}>
+                      <img src={telephone} alt="tlf" width="40" />
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
 
-                    <Card.Title style={{textAlign:"left"}}>Facebook</Card.Title> 
-                        <Row >
-                            <Col > 
-                                <Card.Text style={{textAlign:"left", marginTop:20}}>
-                                yjmendez10@gmail.com
-                                </Card.Text>
-                            </Col> 
-                            <Col style={{paddingTop:10}}> 
-                                <img src={facebook} alt="tlf" width="40" /> 
-                            </Col>  
-                        </Row>
+            <Col></Col>
 
-                    <Card.Title style={{textAlign:"left", marginTop:20}}>Instagram</Card.Title> 
-                        <Row >
-                            <Col > 
-                                <Card.Text style={{textAlign:"left", marginTop:20}}>
-                                yjmendez10@gmail.com
-                                </Card.Text>
-                            </Col> 
-                            <Col style={{paddingTop:10}}> 
-                                <img src={instagram} alt="tlf" width="40" /> 
-                            </Col>  
-                        </Row>
-                    </Card.Body>
-                    <Card.Footer> </Card.Footer>
-                </Card>
-                </Col>
-                
-                </Row>
-                
-               
-          </Container>
-          </section>
-        );
-    }
+            <Col style={styles.card}>
+              <Card style={styles.c}>
+                <Card.Body>
+                  <Card.Title>Facebook</Card.Title>
+                  <Col>
+                    <Card.Text style={{ textAlign: "center", marginTop: 20 }}>
+                      yjmendez10@gmail.com
+                    </Card.Text>
+                  </Col>
+                  <Row>
+                    <Col style={{ marginTop: 20 }}>
+                      <Card.Link
+                        target="_blank"
+                        href="https://www.facebook.com"
+                      >
+                        Facebook
+                      </Card.Link>
+                    </Col>
+                    <Col style={{ paddingTop: 10 }}>
+                      <img src={facebook} alt="tlf" width="40" />
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col></Col>
+
+            <Col style={styles.card}>
+              <Card style={styles.c}>
+                <Card.Body>
+                  <Card.Title>Correros</Card.Title>
+                  <Col>
+                    <Card.Text style={{ textAlign: "center", marginTop: 20 }}>
+                      yjmendez10@gmail.com
+                    </Card.Text>
+                  </Col>
+                  <Row>
+                    <Col style={{ marginTop: 20 }}>
+                      <Card.Link
+                        target="_blank"
+                        href="https://www.google.com/intl/es-419/gmail/about/"
+                      >
+                        Gmail
+                      </Card.Link>
+                    </Col>
+
+                    <Col style={{ paddingTop: 10 }}>
+                      <img src={mail} alt="tlf" width="40" />
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col></Col>
+
+            <Col style={styles.card}>
+              <Card style={styles.c}>
+                <Card.Body>
+                  <Card.Title>Instagram</Card.Title>
+                  <Col>
+                    <Card.Text style={{ textAlign: "center", marginTop: 20 }}>
+                      yjmendez10@gmail.com
+                    </Card.Text>
+                  </Col>
+                  <Row>
+                    <Col style={{ marginTop: 20 }}>
+                      <Card.Link
+                        target="_blank"
+                        href="https://www.instagram.com/?hl=es"
+                      >
+                        Instagram
+                      </Card.Link>
+                    </Col>
+                    <Col style={{ paddingTop: 10 }}>
+                      <img src={instagram} alt="tlf" width="40" />
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
 }
 
 export default Contactos;
 
-
 const styles = {
-    card: {
-      backgroundColor: '#B7E0F2',
-      borderRadius: 55,
-      padding: '3rem'
-    },
-    cardImage: {
-      height: '100%',
-      objectFit: 'cover',
-      borderRadius: 55
-    }
-  };
+  card: {
+    backgroundColor: "#000000",
+    opacity: 0.9,
+    borderRadius: 55,
+    padding: "1rem",
+  },
+  c: {
+    height: "100%",
+    objectFit: "cover",
+    borderRadius: 55,
+  },
+};
