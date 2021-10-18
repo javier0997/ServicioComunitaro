26bf93a0fcd2def68d5afc0f89cfb0188ab7bc54
moises <1128moises@gmail.com>
2021-10-17 21:46:49 -0400

    Refactor(login)

--- servicio_comunitario/src/pages/login.js
+++ servicio_comunitario/src/pages/login.js
@@ -1,44 +1,58 @@
 import React, { Component } from "react";
-
 import { Form, Button, Card } from "react-bootstrap";
+import Background from "../assets/back.jpg";
+
+var sectionStyle = {
+  backgroundSize: "cover",
+  height: "89.3vh",
+  backgroundImage: `url(${Background})`,
+};
 
 class Login extends Component {
   render() {
     return (
-      <div
-        style={{
-          paddingTop: 100,
-          display: "flex",
-          justifyContent: "center",
-          alignContent: "center",
-        }}
-      >
-        <Card style={{ width: "40rem" }}>
-          <Card.Body>
-            <Card.Title>Login</Card.Title>
-            <Card.Text>
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
+      <section style={sectionStyle}>
+        <div
+          style={{
+            paddingTop: 100,
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+        >
+          <Card className="bg-dark" style={{ width: "40rem" }}>
+            <Card.Body>
+              <Card.Title className="text-muted " style={{ fontSize: 40 }}>
+                Login
+              </Card.Title>
+              <br />
+              <br />
+              <Card.Text>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label className="text-muted">
+                      Email address
+                    </Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className="text-muted">Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
 
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button>
-              </Form>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
+                  <Button variant="secondary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </section>
     );
   }
 }
