2eb5804091aafdeb88ab15a1fa33a91c9f9d7406
albertolab <albertolab20@gmail.com>
2022-01-18 17:36:13 -0400

    Revert "Revert "FEATURE: tabla asignaciones(se valida curso y profesor), datosUser a localStorage""
    This reverts commit 200cf3ebbf6829d4e56594981ebac8004c532ec4.

--- servicio_comunitario/src/pages/login.js
+++ servicio_comunitario/src/pages/login.js
@@ -7,7 +7,7 @@ import { useHistory } from "react-router-dom";
 
 
 export const Login = () => {
-  localStorage.setItem("loginSC", "0");
+  localStorage.clear();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
@@ -16,6 +16,7 @@ export const Login = () => {
   const [passwordAux, setPasswordAux] = useState("");
   const [emailAux, setEmailAux] = useState("");
   const [rol, setRol] = useState("");
+  const [curso, setCurso] = useState("");
 
 
   const history = useHistory();
@@ -40,12 +41,19 @@ export const Login = () => {
           setRol(doc.data().rol);
           setEmailAux(doc.data().user);
           setPasswordAux(doc.data().contraseña);
-          console.log(rol+" "+emailAux+" "+passwordAux);
+          setCurso(doc.data().curso);
+          
+          console.log(rol+" "+emailAux+" "+passwordAux+" "+curso);
 
           if (email==emailAux && password==passwordAux ){
             switch (rol) {
               case 'profesor':
-                localStorage.setItem("loginSC", "1");
+                const profesorCaracteristicas = {
+                                                'userSC': `${email}`,
+                                                'cursoSC': `${curso}`,
+                                                'loginSC': "1"
+                                              };
+                localStorage.setItem('datosUser', JSON.stringify(profesorCaracteristicas));
                 history.push("/profesores");
                 break;
               case 'estudiante':
