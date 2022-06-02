c7e0d5c551901e577e36295cbe310a3405a04c0f
albertolab <albertolab20@gmail.com>
2022-06-02 00:00:19 -0400

    Fix: correos en login

--- servicio_comunitario/src/pages/login.js
+++ servicio_comunitario/src/pages/login.js
@@ -6,90 +6,95 @@ import { useHistory } from "react-router-dom";
 import Loading from "../components/Loading";
 
 
-
-
 export const Login = () => {
   localStorage.clear();
+  const history = useHistory();
+  const db = firebase.firestore();
   const [isLoading, setIsLoading] = useState(false);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
 
-  const [passwordAux, setPasswordAux] = useState("");
-  const [emailAux, setEmailAux] = useState("");
-  const [rol, setRol] = useState("");
-  const [curso, setCurso] = useState("");
-  const [nombre, setNombre] = useState("");
-  const [apellido, setApellido] = useState("");
-  const [alerta, setAlert] = useState(false);
-
-
-
-
-  const history = useHistory();
-  const db = firebase.firestore();
-
-  // const dbRef = ref(getDatabase());
-  //   get(child(dbRef, `users/${email}`)).then((snapshot) => {
-  //     if (snapshot.exists()) {
-  //       console.log(snapshot.val());
-  //     } else {
-  //       console.log("No data available");
-  //     }
-  //   }).catch((error) => {
-  //     console.error(error);
-  //   });
   
-  const getUser = async () => {
-    setIsLoading(true)
-    await db.collection('users').where('user', '==', `${email}`)
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          
-          console.log(email + " "+ doc.data().rol+" "+doc.data().user+" "+doc.data().contraseña+" "+doc.data().curso + " "+doc.data().correo );
 
-          if (email==doc.data().user && password==doc.data().contraseña ){
-            switch (doc.data().rol) {
-              case 'profesor':
-                let profesorCaracteristicas = {
-                                                'userSC': `${email}`,
-                                                'nombreSC': `${doc.data().nombre}`,
-                                                'apellidoSC': `${doc.data().apellido}`,
-                                                'cursoSC': `${doc.data().curso}`,
-                                                'rolSC': `${doc.data().rol}`
-                                              };
-                localStorage.setItem('datosUser', JSON.stringify(profesorCaracteristicas));
-                history.push("/profesores");
-                setIsLoading(false)
-                break;
-              case 'estudiante':
-                let estudianteCaracteristicas = {
-                                            'userSC': `${email}`,
-                                            'nombreSC': `${doc.data().nombre}`,
-                                            'apellidoSC': `${doc.data().apellido}`,
-                                            'cursoSC': `${doc.data().curso}`,
-                                            'rolSC': `${doc.data().rol}`
-                                          };
-                localStorage.setItem('datosUser', JSON.stringify(estudianteCaracteristicas));
-                history.push("/estudiantes");
-                setIsLoading(false)
-                break;
-              case 'administrador':
-                localStorage.setItem("loginSC", "3");
-                break;
-
-            }
-          }else {
-           alert("Error: contraseña o usuario incorrectos.")
-           setIsLoading(false)
-          }
-
-        });
+  const loginFirebase = async () => {
+      setIsLoading(true)
+      console.log(email+"-"+password)
+      await firebase.auth().signInWithEmailAndPassword(email,password).then((userCredential) => {
+        // Signed in
+       
+       let lastIndex = email.lastIndexOf('@')
+       let userName = email.slice(0,lastIndex)
+       getUser(userName)
+       console.log(userName)
+        const user = userCredential.user;
+        
+        // ...
       })
-      
+      .catch((error) => {
+        setIsLoading(false)
+        alert("Contraseña o correo invalido.")
+        console.log(error)
+        setEmail("")
+        setPassword("")
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
+
+  
+  const getUser = async (user) => {
+           await db.collection('users').where('user', '==', `${user}`)
+            .get()
+            .then(querySnapshot => {
+              querySnapshot.forEach(doc => {
+              
+                console.log(email + " "+ doc.data().rol+" "+doc.data().user+" "+doc.data().contraseña+" "+doc.data().curso + " "+doc.data().correo );
+
+                if ( password==doc.data().contraseña ){
+                  switch (doc.data().rol) {
+                    case 'profesor':
+                      let profesorCaracteristicas = {
+                                                      'userSC': `${doc.data().user}`,
+                                                      'nombreSC': `${doc.data().nombre}`,
+                                                      'apellidoSC': `${doc.data().apellido}`,
+                                                      'cursoSC': `${doc.data().curso}`,
+                                                      'rolSC': `${doc.data().rol}`
+                                                    };
+                      localStorage.setItem('datosUser', JSON.stringify(profesorCaracteristicas));
+                      history.push("/profesores");
+                      setIsLoading(false)
+                      break;
+                    case 'estudiante':
+                      let estudianteCaracteristicas = {
+                                                   'userSC': `${doc.data().user}`,
+                                                  'nombreSC': `${doc.data().nombre}`,
+                                                  'apellidoSC': `${doc.data().apellido}`,
+                                                  'cursoSC': `${doc.data().curso}`,
+                                                  'rolSC': `${doc.data().rol}`
+                                                };
+                      localStorage.setItem('datosUser', JSON.stringify(estudianteCaracteristicas));
+                      history.push("/estudiantes");
+                      setIsLoading(false)
+                      break;
+                    case 'administrador':
+                      localStorage.setItem("loginSC", "3");
+                      break;
+
+                  }
+                }else {
+                alert("Error: contraseña o usuario incorrectos.")
+                setIsLoading(false)
+                }
+
+              });
+            }).catch((error) => {
+              console.log(error)
+              alert(error.message)
+              const errorCode = error.code;
+              const errorMessage = error.message;
+            });
+             
   };
 
   return (
@@ -104,48 +109,53 @@ export const Login = () => {
         <Col>
           <Row>
             <div style={{ paddingTop: 100 }}>
-              <Card className="bg-white">
+              <Card className="bg-white border-primary">
                 <Card.Body>
                   <Card.Title className="text-muted " style={{ fontSize: 40, marginBottom:40 }}>
                     Inicio de Sesion
                   </Card.Title>
-                  <Card.Text>
-                    <Form>
-                      <Form.Group className="mb-3" >
-                        <Form.Control
+                  <Card.Text >
+                  
+                  <div className="form-outline mb-4">
+                        <input
+                         style={{ fontSize: 16 }}
+                         className="form-control border-primary" 
                           type="email"
                           id="email"
                           value={email}
                           onChange={(e) => setEmail(e.target.value)}
-                          placeholder="Nombre de usuario"
+                          placeholder="Correo electronico"
                           size="lg"
+                          onKeyPress={(e) => e.key === 'Enter' && loginFirebase()}
                         />
-                      </Form.Group>
+                     </div>
 
-                      <Form.Group
-                        className="mb-3"
-                      >
-                        <Form.Control
+                     <div className="form-outline mb-4">
+                        <input
+                        style={{ fontSize: 16 }}
+                        className="form-control border-primary" 
                           type="password"
                           id="password"
                           value={password}
                           onChange={(e) => setPassword(e.target.value)}
                           placeholder="Contraseña"
                           size="lg"
+                          onKeyPress={(e) => e.key === 'Enter' && loginFirebase()}
                         />
-                      </Form.Group>
+                     </div>
 
-                      <div style={{ paddingRight: 510 }}>
+                      <div style={{ alignItems:'center' }}>
                         <Button
                           variant="primary"
                           type="submit"
-                          className="text-white"
-                          onClick={getUser}
+                          className="text-white btn-lg"
+                          onClick={loginFirebase}
                         >
                           Iniciar sesion
                         </Button>
                       </div>
-                    </Form>
+                
+                   
                   </Card.Text>
                 
                 </Card.Body>
